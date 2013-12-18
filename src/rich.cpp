35a4baa081fe3c6c43d58df25d3b048ca5993a35
Christian Loose <christian.loose@hamburg.de>
2013-12-18 16:17:49 +0100

    New class Rich
    Added new class Rich which represents the resource type "rich"

---
+++ src/rich.cpp
@@ -0,0 +1,80 @@
+#include "rich.h"
+
+#include <QString>
+
+namespace qoembed {
+
+class RichPrivate
+{
+public:
+    RichPrivate() : width(0), height(0) {}
+
+    QString html;
+    unsigned width;
+    unsigned height;
+};
+
+Rich::Rich() :
+    Response(),
+    d(new RichPrivate)
+{
+}
+
+Rich::Rich(const Rich &other) :
+    Response(other)
+{
+    d = new RichPrivate;
+    d->html = other.d->html;
+    d->width = other.d->width;
+    d->height = other.d->height;
+}
+
+Rich::~Rich()
+{
+    delete d;
+}
+
+Rich &Rich::operator=(const Rich &rhs)
+{
+    // no self-assignment
+    if (this != &rhs) {
+        Response::operator =(rhs);
+        d->html = rhs.d->html;
+        d->width = rhs.d->width;
+        d->height = rhs.d->height;
+    }
+
+    return *this;
+}
+
+void Rich::setHtml(const QString &html)
+{
+    d->html = html;
+}
+
+QString Rich::html() const
+{
+    return d->html;
+}
+
+void Rich::setWidth(unsigned width)
+{
+    d->width = width;
+}
+
+unsigned Rich::width() const
+{
+    return d->width;
+}
+
+void Rich::setHeight(unsigned height)
+{
+    d->height = height;
+}
+
+unsigned Rich::height() const
+{
+    return d->height;
+}
+
+} // namespace qoembed
---
+++ src/rich.h
@@ -0,0 +1,35 @@
+#ifndef QOEMBED_RICH_H
+#define QOEMBED_RICH_H
+
+#include "qoembed_global.h"
+#include "response.h"
+
+namespace qoembed {
+
+class RichPrivate;
+
+class QOEMBEDSHARED_EXPORT Rich : public Response
+{
+public:
+    Rich();
+    Rich(const Rich &other);
+    ~Rich();
+
+    Rich &operator=(const Rich &rhs);
+
+    void setHtml(const QString &html);
+    QString html() const;
+
+    void setWidth(unsigned width);
+    unsigned width() const;
+
+    void setHeight(unsigned height);
+    unsigned height() const;
+
+private:
+    RichPrivate *d;
+};
+
+} // namespace qoembed
+
+#endif // QOEMBED_RICH_H
